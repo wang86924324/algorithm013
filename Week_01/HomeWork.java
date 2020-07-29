a553ded822c6bb4979fb785df5927b6ee5fbef1c
wang86924324 <wang86924324@163.com>
2020-07-30 00:35:40 +0800

    submit HomeWork.java

--- Week_01/HomeWork.java
+++ Week_01/HomeWork.java
@@ -1,5 +1,10 @@
 package Week_01;
 
+import common.ListNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class HomeWork {
 
     // https://leetcode-cn.com/problems/rotate-array/
@@ -20,5 +25,161 @@ public class HomeWork {
             if (i > 0 && nums[i] == nums[i - 1]) nums[++slow] = nums[i];
         }
         return slow + 1;
+    }
+
+    // 一般有一个dummy指针，dummy指向第一个节点，返回dummy.next
+    // 一个指针维护结果列表，另外两个指针负责变量（l1和l2是现成的）
+    // https://leetcode-cn.com/problems/merge-two-sorted-lists/
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode pre = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                pre.next = l1;
+                l1 = l1.next;
+            } else {
+                pre.next = l2;
+                l2 = l2.next;
+            }
+        }
+
+        pre.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
+
+    /**
+     * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+     * 说明:
+     * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+     * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+     *
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+
+    // 合并的长度是m+n，所以从m+n-1开始往前方数组，比较的数组从m-1的位置和n-1的位置开始。
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int k = m + n - 1, i = m - 1, j = n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j])
+                nums1[k--] = nums1[i--];
+            else
+                nums1[k--] = nums2[j--];
+        }
+
+        while (i >= 0) nums1[k--] = nums1[i--];
+        while (j >= 0) nums1[k--] = nums1[j--];
+    }
+
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * <p>
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     * <p>
+     *  
+     * <p>
+     * 示例:
+     * <p>
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     * <p>
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/two-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null) return null;
+
+        // key:target-nums[j]  value:index
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(target - nums[i], i);
+        }
+
+        return null;
+    }
+
+
+    /**
+     * 283. 移动零
+     * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: [0,1,0,3,12]
+     * 输出: [1,3,12,0,0]
+     * 说明:
+     * <p>
+     * 必须在原数组上操作，不能拷贝额外的数组。
+     * 尽量减少操作次数。
+     *
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        // 快慢指针
+        // 慢指针：左边都不为0，慢指针在0处停止前进
+        // 快指针：为0就往前跑，遇到不为0的数字，然后跟慢指针的数字交换，交换完成之后都往前进一步。
+        // 初始状态：开始位置。
+        // 一般便利快指针。
+
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != 0) {
+                swap(nums, slow, fast);
+                slow++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+
+    /**
+     * 66. 加一
+     * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+     * <p>
+     * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+     * <p>
+     * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: [1,2,3]
+     * 输出: [1,2,4]
+     * 解释: 输入数组表示数字 123。
+     * 示例 2:
+     * <p>
+     * 输入: [4,3,2,1]
+     * 输出: [4,3,2,2]
+     * 解释: 输入数组表示数字 4321。
+     *
+     * @param digits
+     * @return
+     */
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length; i >= 0; i++) {
+            digits[i]++;
+            digits[i] = digits[i] % 10;
+            if (digits[i] != 0) return digits;
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
     }
 }
