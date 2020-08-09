1f1fd95608d486525f7d9fc0b94cee9f90039606
wang86924324 <wang86924324@163.com>
2020-08-09 16:54:44 +0800

    submit 提交作业。。。。。

--- Week_02/HomeWork.java
+++ Week_02/HomeWork.java
@@ -1,9 +1,7 @@
 package Week_02;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
 
 public class HomeWork {
     // https://leetcode-cn.com/problems/valid-anagram/description/
@@ -32,12 +30,11 @@ public class HomeWork {
 
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) return new int[]{map.get(nums[i]),i};
+            if (map.containsKey(nums[i])) return new int[]{map.get(nums[i]), i};
             map.put(target - nums[i], i);
         }
         return new int[0];
     }
-
 
 
     // https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/submissions/
@@ -53,6 +50,7 @@ public class HomeWork {
         }
         return result;
     }
+
     // Definition for a Node.
     static class Node {
         public int val;
@@ -69,5 +67,66 @@ public class HomeWork {
             val = _val;
             children = _children;
         }
+    }
+
+    // 1.先排序,生成key，再分类 NKlogK  数组中最大子字符串的长度
+    // 2.计算位置，生成key，再分类
+    // https://leetcode-cn.com/problems/group-anagrams/
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList<>();
+//        // 实现2：先排序，再分组  假设数组的中字符串最大长度为K，排序的时间复杂度是KlogK
+//        Map<String, List<String>> map = new HashMap<>();
+//        for (String s : strs) {
+//            char[] chars = s.toCharArray();
+//            Arrays.sort(chars);
+//            String key = String.valueOf(chars);
+//            if (!map.containsKey(key)) map.put(key, new ArrayList());
+//            map.get(key).add(s);
+//        }
+//        return new ArrayList<>(map.values());
+
+        // 实现3：生成key的规则，位置数组
+        Map<String, List<String>> map = new HashMap<>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!map.containsKey(key)) map.put(key, new ArrayList());
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    // https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+    // 1.递归
+    // 2.栈
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inOrder(root, res);
+        return res;
+    }
+
+    public void inOrder(TreeNode treeNode, List<Integer> res) {
+        if (treeNode == null) return;
+        inOrder(treeNode.left, res);
+        res.add(treeNode.val);
+        inOrder(treeNode.right, res);
     }
 }
